2cb7c4909febe984684955bd548348466f8b7895
PedroAntonio <antonio.pedro25@outlook.com>
2024-11-24 17:25:48 -0300

    Modificando connection.php, enviando dados no formato JSON

--- backend/connection.php
+++ backend/connection.php
@@ -1,5 +1,5 @@
 <?php
-
+    header('Content-Type: application/json');
     $nota = $_POST['nota'];
 
     $conexao = mysqli_connect("localhost", "root", "", "tarefadb");
@@ -10,11 +10,43 @@
 
     $sql = "INSERT INTO tarefas (nota) VALUES ('$nota')";
 
-    if (mysqli_query($conexao, $sql)) {
-        print "Dados cadastrados com sucesso";
-    } else {
-        print "Error: " . $sql . "<br>" . mysqli_error($conexao);
+    $sqlResult = "SELECT * FROM tarefas";
+
+    $result = mysqli_query($conexao, $sqlResult);
+
+    // if (mysqli_query($conexao, $sql)) {
+    //     // print "Dados cadastrados com sucesso";
+    //     header('Location: ../index.html');
+    // } else {
+    //     print "Error: " . $sql . "<br>" . mysqli_error($conexao);
+    // }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nota'])) {
+        $nota = mysqli_real_escape_string($conexao, $_POST['nota']);
+    
+        // Insere a nota no banco
+        $sql = "INSERT INTO tarefas (nota) VALUES ('$nota')";
+        if (!mysqli_query($conexao, $sql)) {
+            echo json_encode(["erro" => "Erro ao inserir nota: " . mysqli_error($conexao)]);
+            exit;
+        }
+        header('Location: ../index.html');
     }
+
+    if ($result) {
+        $tarefas = [];
+    
+        // Loop para coletar os resultados em um array
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tarefas[] = $row;
+        }
+    
+        // Retorna as notas no formato JSON
+        echo json_encode($tarefas);
+    } else {
+        echo json_encode(["erro" => "Erro ao buscar notas: " . mysqli_error($conexao)]);
+    }
+    
 
     mysqli_close($conexao);
 ?>
